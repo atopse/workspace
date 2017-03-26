a574141f34a6ffff23d09bd9aa674154d986413c
ysqi <devysq@gmail.com>
2017-03-26 13:57:43 +0800

    菜单页Active处理

--- src/client/app/shared/sidebar/sidebar.ts
+++ src/client/app/shared/sidebar/sidebar.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
 	moduleId: module.id,
@@ -6,17 +7,17 @@ import { Component } from '@angular/core';
 	templateUrl: 'sidebar.html'
 })
 
-export class SidebarComponent {
-	isActive = false;
-	showMenu: string = '';
-	eventCalled() {
-		this.isActive = !this.isActive;
-	}
-	addExpandClass(element: any) {
-		if (element === this.showMenu) {
-			this.showMenu = '0';
-		} else {
-			this.showMenu = element;
-		}
-	}
+export class SidebarComponent { 
+	constructor(private router:Router){
+
+	}  
+	/**
+	 * 判断是否该路由已被Active，使用非精准模式匹配。
+	 * @param url 菜单路由地址
+	 */
+	isActive(url: string): boolean {
+		return this.router.isActive(url,false);
+  	} 
 }
+
+// TODO：需要实现点击菜单时，自动合并/展开子菜单，现在是默认全部显示
