14747a85fc6d37e6fffa342c865aab1b0c9abd3e
ysqi <devysq@gmail.com>
2017-04-15 16:48:15 +0800

    修改协议

--- src/client/app/dashboard/dashboard.routes.ts
+++ src/client/app/dashboard/dashboard.routes.ts
@@ -1,13 +1,13 @@
 import { Route } from '@angular/router';
- 
+
 import { DashboardComponent } from './index';
 
 export const DashboardRoutes: Route[] = [
-  	{
-    	path: 'dashboard',
-    	component: DashboardComponent,
-    	children: [
-        {path:"demo",loadChildren:'app/dashboard/demo/demo.module#DemoModule'}, 
-    	]
-  	}
+	{
+		path: 'dashboard',
+		component: DashboardComponent,
+		children: [
+			{ path: "demo", loadChildren: 'app/dashboard/demo/demo.module#DemoModule' },
+		]
+	}
 ];
